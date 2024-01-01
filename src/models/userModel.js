47926df3df370f54bc08ea3005d916a32b9ba4bc
soruj <mathsoruj@gmail.com>
2024-01-01 11:41:07 +0600

    model schema add

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,17 +1,59 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+const { defaultImage } = require("../secret");
 
-const users = [
-    {
-      id: 1,
-      name: "soruj",
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is Miss"],
+      trim: true,
+      minlength: [3, "the length of user name can be minimum 3 characters"],
+      maxlength: [31, "the length of user name can be maximum 31 characters"],
     },
-    {
-      id: 2,
-      name: "lakhi",
+    email: {
+      type: String,
+      required: [true, "Email is Miss"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      validate: {
+        validator: function (v) {
+          return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(v);
+        },
+        message: "Please Enter a valid Email !",
+      },
     },
-    {
-      id: 3,
-      name: "Nahida",
+    password: {
+      type: String,
+      required: [true, "Password is Miss"],
+      minlength: [6, "the length of user name can be minimum 6 characters"],
+      set: v => bcrypt.hashSync(v, bcrypt.genSaltSync(10)),
     },
-  ];
-  
-  module.exports = users
+    image: {
+        type: String,
+        default: defaultImage,
+    },
+    address: {
+      type: String,
+      required: [true, "Address is Miss"],
+    },
+    phone: {
+      type: String,
+      required: [true, "Address is Miss"],
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    isBanned: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("Users", userSchema);
+
+module.exports = User;
--- src/secret.js
+++ src/secret.js
@@ -1,9 +1,10 @@
 require("dotenv").config();
 const PORT = process.env.SERVER_PORT
 const mongodbURL = process.env.MONGODB_ATLAS_URL ||" mongodb://localhost:27017/ecommerceMernDb2024"
-
+const defaultImage = "../public/images/users/logo.png"
 
 module.exports = {
     PORT,
-    mongodbURL
+    mongodbURL,
+    defaultImage
 }
